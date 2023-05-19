babd72e0fc0c80a1ba68028782881d12089a2aad
Javier Vargas <ijavom@gmail.com>
2023-05-18 21:18:12 -0500

    Generacion logica y diseño de titulo

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -1,1 +1,12 @@
-alert("Hola mundi!");
+var cine = {
+    nombre : "Cine JS UNC"
+}
+
+
+var ponerTituloPagina = () => {
+    var tituloPagina = document.querySelector("#titulo-pagina");
+    tituloPagina.textContent = cine.nombre;
+}
+
+
+ponerTituloPagina();
